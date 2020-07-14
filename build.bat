0575b530b56d2afce0e1c6e3d09f6179f26faa29
Ahmad <34627272+AhmadHrb@users.noreply.github.com>
2020-07-14 23:51:11 +0300

    Delete build.bat

--- build.bat
+++
@@ -1,8 +0,0 @@
-echo off
-echo ========
-echo Pocket Browser Builder
-echo Are you sure you want to build Pocket Browser?
-pause
-electron-packager ./ --asar --app-copyright Pocket,Inc. --icon ./icon.ico
-echo Done.
-pause
