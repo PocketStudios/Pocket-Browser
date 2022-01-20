ecba007fdcc1814a6cadf5d5ce37e33bee5cc8d2
AhmadHrb <ahmad@pocket-inc.com>
2022-01-20 21:34:28 +0200

    Bug Fix

--- src/URLBar/changeURL.js
+++ src/URLBar/changeURL.js
@@ -5,7 +5,7 @@ if (tab == tabGroup.getActiveTab()) {
             return;
         }
     }
-    document.getElementById("address").value = event.url;
+    if (!document.activeElement || document.activeElement.id !== "address") document.getElementById("address").value = event.url;
     changeSecure()
     if (tab.webview.src.startsWith("file:///")) return;
         checkHistory(event.url)
