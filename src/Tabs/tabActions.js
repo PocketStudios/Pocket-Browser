520aa1bac2ad6a0f84513e92d9f9eed1ca41dbd4
AhmadHrb <ahmad@pocket-inc.com>
2022-01-20 21:58:37 +0200

    Added FullScreen/Zoom

--- src/Tabs/tabActions.js
+++ src/Tabs/tabActions.js
@@ -6,3 +6,22 @@ if (currentTab.isDevToolsOpened()) {
     currentTab.openDevTools()
 }
 }
+function toggleFullScreen() {
+        let newState = !require("@electron/remote").getCurrentWindow().isFullScreen();
+        require("@electron/remote").getCurrentWindow().setFullScreen(newState);
+        if (newState == false) {
+            document.getElementById("fullScreen").src = "node_modules/bootstrap-icons/icons/fullscreen.svg"
+        } else {
+            document.getElementById("fullScreen").src = "node_modules/bootstrap-icons/icons/fullscreen-exit.svg"
+        }
+}
+
+function zoom(type) {
+    if (type == "add") {
+        var newZoom = tabGroup.getActiveTab().webview.getZoomLevel() + 0.5;
+        tabGroup.getActiveTab().webview.setZoomLevel(newZoom)
+    } else if (type == "minus") {
+        var newZoom = tabGroup.getActiveTab().webview.getZoomLevel() - 0.5;
+        tabGroup.getActiveTab().webview.setZoomLevel(newZoom)
+    }
+}
