ac6224b86a6d90eda3290e9199a13f214b4bdbc4
AhmadHrb <ahmadharb2115@gmail.com>
2023-01-29 21:16:48 +0200

    removed system pages

--- src/System/pages/dev.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener("load",function () {
-document.getElementById('pocket-dev-tools').addEventListener('click',function () {require("@electron/remote").remote.BrowserWindow.getAllWindows().forEach(window => window.openDevTools())})
-})
